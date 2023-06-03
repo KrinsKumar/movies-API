f6845f5706217bba9f79cabb06bc59d66b1286f7
Krins <krinskumar@gmail.com>
2023-06-03 17:06:51 -0400

    a3-starte

---
+++ my-app/components/PageHeader.js
@@ -0,0 +1,7 @@
+export default function PageHeader({ text }) {
+    return (
+        <p>
+            Page Header
+        </p>
+    )
+}
---
+++ my-app/pages/about.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Card from "react-bootstrap";
+import MovieDetails from "@/components/MovieDetails";
+import PageHeader from "@/components/PageHeader";
+
+export function getStaticProps() {
+    return new Promise((resolve, reject) => {
+        fetch 
+    });
+}
+
+export default function About() {
+    return (
+        <p>
+            About
+        </p>
+    )
+}
