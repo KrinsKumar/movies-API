bc91fa815aa2d75ab7279de5803a0227eea83b34
Krins <krinskumar@gmail.com>
2023-06-06 09:56:24 -0400

    added-console

--- modules/moviesDB.js
+++ modules/moviesDB.js
@@ -59,7 +59,7 @@ module.exports = class MoviesDB {
       );
 
       db.once('error', (err) => {
-        console.log("close " + err)
+         //console.log("close " + err)
         reject(err);
       });
       db.once('open', () => {
